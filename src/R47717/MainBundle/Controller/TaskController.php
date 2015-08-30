457bcca5f1621698c84ffa5e7f3974174043dea9
r47717 <mchr@yandex.ru>
2015-08-30 15:47:41 +0300

    statistics added

--- src/R47717/MainBundle/Controller/TaskController.php
+++ src/R47717/MainBundle/Controller/TaskController.php
@@ -236,6 +236,50 @@ class TaskController extends Controller
         ]);
     }
 
+    /**
+     * @Route("/stat", name="show_stat")
+     */
+    public function showStat() {
+
+        $entities = $this->getDoctrine()->getManager()->getRepository('R47717MainBundle:Task')->findAll();
+
+        $all = count($entities);
+        $completed = 0; 
+        $completed_ontime = 0; 
+        $pending = 0;
+        $pending_ontime = 0;
+        $today = new \DateTime('today');
+
+        foreach ($entities as $entity) {
+            if ($entity->getCompleted()) {
+                $completed++;
+                $completed_ontime++;
+            } else {
+                $pending++;
+                if ($entity->getDue() > $today) {
+                    $pending_ontime++;
+                }
+            }
+        }
+
+        $completed_overdue = $completed - $completed_ontime;
+        $pending_overdue = $pending - $pending_ontime;
+
+        $stat = [
+            'all' => $all,
+            'completed' => $completed,
+            'completed_ontime' => $completed_ontime,
+            'completed_overdue' => $completed_overdue,
+            'pending' => $pending,
+            'pending_ontime' => $pending_ontime, 
+            'pending_overdue' => $pending_overdue,
+        ];
+
+        return $this->render('R47717MainBundle:Task:stat.html.twig', [
+            'stat' => $stat,
+        ]);
+    }
+
 }
 
 
